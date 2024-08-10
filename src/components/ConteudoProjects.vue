38440f3fd30dfe64729f16a0083f8ae8cae3016e
Georg Herison <georgherison@gmail.com>
2024-08-10 06:49:32 -0300

    projects started

---
+++ src/components/ConteudoProjects.vue
@@ -0,0 +1,17 @@
+<template>
+   
+   <section>
+    
+   </section>
+</template>
+
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+
+
+    export default defineComponent({
+        name: 'ConteudoProjects'
+    })
+</script>
